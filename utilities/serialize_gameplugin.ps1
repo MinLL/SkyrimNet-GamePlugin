8033b94ae6334bc1da8e68c780858832f690319c
langfod <langfod@users.noreply.github.com>
2026-05-01 09:48:05 -0400

    Update spriggit version and plugin

--- utilities/serialize_gameplugin.ps1
+++ utilities/serialize_gameplugin.ps1
@@ -1,9 +1,16 @@
 
-. .\Build_Config_Template.ps1
-$spriggitVersion = "0.40.0"
+# Extract SpriggitCLI.zip to the "external" directory if it doesn't already exist
+# from: "https://github.com/Mutagen-Modding/Spriggit/releases/download/0.40.1/SpriggitCLI.zip",
+
+
+$spriggitVersion = "0.40.1"
 $pluginName = "SkyrimNet"
 $plugintype = "esp"
 
+#  Create a file called "Build_Config_Local.ps1"
+# its should set the following variables:
+# $gameDataPath - path to Skyrim SE Data directory containing the plugin to serialize 
+# $gameDataPath = "D:\SteamLibrary\steamapps\common\Skyrim Special Edition\Data"
 if (Test-Path .\Build_Config_Local.ps1) {
     . .\Build_Config_Local.ps1
 }
@@ -17,7 +24,7 @@ if (-not (Test-Path $spriggitExe)) {
     Write-Error "Spriggit CLI not found at $spriggitExe"
     exit 1
 }
-& $spriggitExe convert-from-plugin -InputPath "$gameDataPath\$pluginName.$plugintype" -OutputPath "plugins\$pluginName" -GameRelease SkyrimSE -p Spriggit.Json -v $spriggitVersion
+& $spriggitExe convert-from-plugin --InputPath "$gameDataPath\$pluginName.$plugintype" --OutputPath "plugins\$pluginName" --GameRelease SkyrimSE -p Spriggit.Json -v $spriggitVersion
 if ($LASTEXITCODE -ne 0) {
     Write-Error "Spriggit CLI failed with exit code $LASTEXITCODE"
     exit $LASTEXITCODE
